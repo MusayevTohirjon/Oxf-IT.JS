4d303123e8ef16e0137d27d3e1bf9f6b1d00e6b3
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-05-31 15:57:35 +0500

    changed

---
+++ Lessons/2-dars/index.js
@@ -0,0 +1,70 @@
+let number = 2012
+console.log(number)
+console.log(typeof number);
+
+// =============================
+
+let bigInt = BigInt(2 ** 53) + 1n;
+console.log(bigInt);
+console.log(typeof bigInt);
+
+// =============================
+
+let name = `Eshmat`
+console.log(name);
+console.log(typeof name);
+
+// =============================
+
+let boolean = true;
+console.log(boolean);
+console.log(typeof boolean);
+
+// ==============================
+
+let id = 10
+id = null
+console.log(id);
+console.log(typeof id);
+
+// ===============================
+
+let name2
+console.log(name2);
+console.log(typeof name2);
+
+// ===============================
+
+let id1 = Symbol()
+console.log(Symbol);
+console.log(typeof Symbol);
+
+// ===============================
+
+// nonPrimitive:Objects:1.object,2.array,3.function
+
+let user = {
+    id: 29989,
+    name: Toshmat,
+    age: 35
+}
+console.log(user["age"]);
+console.log(typeof user);
+
+// ==============================
+
+let arr = [1, "Eshmat", true]
+console.log(arr[2]);
+
+// ==============================
+
+
+
+
+
+
+
+
+
+
+
