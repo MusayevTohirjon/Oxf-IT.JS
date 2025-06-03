6327409c5a4cd270918d5b905fe3e4f68f64cb50
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-06-03 13:26:10 +0500

    changed

---
+++ Homework/2-dars/index.js
@@ -0,0 +1,62 @@
+// let yosh = 25;
+// let kasr = 3.14;
+
+// // =====================================
+
+// let kattaSon = 1234567890123456789012345678901234567890n;
+// let yanakatta = BigInt("98765432109876543210");
+
+// // =====================================
+
+// let ism = "Ali";
+// let salom = 'Salom!';
+
+// // =====================================
+
+// let haqiqat = true;
+// let yolgon = false;
+
+// // =====================================
+
+// let qiymat = null;
+// let foydalanuvchi = null;
+
+// // =====================================
+
+// let toplanmagan;
+// let test = undefined;
+
+// // ====================================
+
+// let id = Symbol("id");
+// let token = Symbol("token");
+
+
+// ========================================
+// ========================================
+// ========================================
+
+// let odam = {
+//     ism: "Ali",
+//     yosh: 30
+// };
+
+// let mashina = {
+//     model: "BMW",
+//     yil: 2020
+// };
+
+// // ==============================
+
+// let raqamlar = [1, 2, 3, 4, 5];
+// let ismlar = ["Ali", "Vali", "Sami"];
+
+// // ============================
+
+// function salomBer() {
+//     console.log("Salom, dunyo!");
+// }
+
+// const kvadrat = function (x) {
+//     return x * x;
+// };
