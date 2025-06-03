c36bc5bb0263b89b56be1e7239d7cd5717235622
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-06-03 18:44:47 +0500

    changed

---
+++ Lessons/3-dars/index.js
@@ -0,0 +1,52 @@
+let a = 20937
+console.log(a + "8337");
+let b = 123
+console.log("283" - b);
+
+console.log(10 + 10);
+console.log(63 + 35);
+
+console.log(10 - 10);
+console.log(3838 - 333);
+
+console.log(10 * 10);
+console.log(10 * 38);
+
+console.log(10 / 10);
+console.log(10 / 63);
+
+console.log(10 % 4);
+console.log(5 % 2);
+
+console.log(10(35 + 47) - 10);
+console.log((1838 * 18) = 83 - 10);
+
+let c = 5
+c = null
+console.log(10 = 10);
+
+console.log(++10);
+console.log(10++);
+
+console.log(10--);
+console.log(--10);
+
+console.log(10 ** 10);
+console.log(10 ** 53);
+
+
+let d = 536
+let h = "733"
+
+d == h
+d === h
+
+console.log(47 < 83);
+console.log(3773 > 3839);
+
+console.log(253 <= 832);
+console.log(374 >= 376);
+
+
+
+
