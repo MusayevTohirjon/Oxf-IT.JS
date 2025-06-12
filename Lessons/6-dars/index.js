d3cb053799d437a70b7bb2f512061bb230c5c437
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-06-12 15:48:13 +0500

    changed

---
+++ Lessons/6-dars/index.js
@@ -0,0 +1,21 @@
+// for (let a = -100; a < 101; a++){
+//     if ( a % 5 === 0) console.log(a);
+    
+// }
+
+// let a = -100;
+// while(a < 101){
+//     if (a % 5 === 0) 
+//     console.log(a);
+//     a++
+// }
+
+
+let a = -100;
+do{
+    if ( a % 5 === 0 && a % 3 === 0) 
+        console.log(a);
+        a++
+}while(a<101)
+
+
