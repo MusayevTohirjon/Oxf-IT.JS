40a58a54852377b09cbf11eb785b0bf30feecde0
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-06-15 18:19:36 +0500

    changed

---
+++ Homework/7-dars/index.js
@@ -0,0 +1,27 @@
+// var n = +prompt("Nechta son kiritmoqchisiz?");
+// var i = 1;
+// var yigindi = 0;
+
+// while (i <= n) {
+//     var son = +prompt(i + "-sonni kiriting:");
+//     yigindi = yigindi + son;
+//     i = i + 1;
+// }
+
+// var arifmetik = yigindi / n;
+
+// alert("O‘rtacha arifmetik: " + arifmetik);
+
+
+
+
+
+// var a = +prompt("1-sonni kiriting:");
+// var b = +prompt("2-sonni kiriting:");
+// var c = +prompt("3-sonni kiriting:");
+
+// var kopaytma = a * b * c;
+
+// var geometrik = kopaytma ** (1 / 3); 
+
+// alert("O‘rtacha geometrik: " + geometrik);
