a4425fca9d8ead820e5cbabae07301a5d3a48dac
MusayevTohirjon <musayevtohirjon933@gmail.com>
2025-07-15 12:49:15 +0500

    changed

---
+++ Homework/14-dars/index.js
@@ -0,0 +1,44 @@
+// let fruits = ["Apples", "Pear", "Orange"];
+// let shoppingCart = fruits;
+// shoppingCart.push("Banana");
+// alert(fruits.length);
+
+
+// let styles = ["Jazz", "Blues"];
+// console.log(styles);
+
+// styles.push("Rock-n-Roll");
+// console.log(styles);
+
+// let middleIndex = Math.floor(styles.length / 2);
+// styles[middleIndex] = "Classics";
+// console.log(styles);
+
+// let firstRemoved = styles.shift();
+// console.log(firstRemoved);
+// console.log(styles);
+
+// styles.unshift("Rap", "Reggae");
+// console.log(styles);
+
+
+
+// let arr = ["a", "b"];
+// arr.push(function () { alert(this); });
+// arr[2]();
+
+
+
+// function sumInput() {
+//     let numbers = [];
+//     for (; ;) {
+//         let value = prompt("Enter a number", "");
+//         if (value === null  value === ""!isFinite(value)) break;
+//         numbers.push(+value);
+//     }
+//     let sum = 0;
+//     for (let number of numbers) sum += number;
+//     return sum;
+// }
+// alert("Sum: " + sumInput());
+
